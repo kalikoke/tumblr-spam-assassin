59c7d853528314745a30c280872e500f8125d821
kalikoke <kalikoke@gmail.com>
2016-06-06 02:25:47 -0500

    Make sure list is unique, and add print statements

--- tumblr_spam_assassin/findr.py
+++ tumblr_spam_assassin/findr.py
@@ -10,18 +10,29 @@ class Findr:
         self.tumblr = Tumblr(self.api_key)
 
     def find_spam_blogs(self, popular_tag, spammer_username=EXAMPLE_SPAMMER):
+        print "Finding spammers in %s" % popular_tag
         self.get_common_link_urls(spammer_username)
         results = self.tumblr.tagged(popular_tag)
         for blog in results:
+            print ".",
             if "link_url" in blog.keys():
                 if self.link_baseurl(blog["link_url"]) in self.link_urls:
                     self.spam_blogs.append(blog["blog_name"])
 
-        return list(set(self.spam_blogs))
+        self.trim_spam_blogs()
+        print "Found: %d spammers!" % (len(self.spam_blogs))
+        return self.spam_blogs
 
     def get_common_link_urls(self, spammer_username=EXAMPLE_SPAMMER):
         for post_data in self.tumblr.posts(spammer_username):
             self.link_urls.append(self.link_baseurl(post_data["link_url"]))
 
+        list(set(self.link_urls))
+
     def link_baseurl(self, link_url):
         return link_url.split("/")[2]
+
+    def trim_spam_blogs(self):
+        self.spam_blogs = list(set(self.spam_blogs))
+
+
