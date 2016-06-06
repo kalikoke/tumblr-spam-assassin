d786ea4fedbae32b0d82145f04044b216e4b6c7c
kalikoke <kalikoke@gmail.com>
2016-06-06 00:20:16 -0500

    Use iPython and updated instructions.

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
 # Setup
 
-
 ``` shell
 docker build -t blockr/dev .
-docker run --rm -v "$PWD":/usr/src/app -it blockr/dev python
+docker run --rm -v "$PWD":/usr/src/app -it blockr/dev ipython
 ```
+
+# In IPython:
+
+```
+%load_ext autoreload
+%autoreload 2
+from blockr import Tumblr
+tumblr = Tumblr(YOUR_API_KEY)
+tumblr.posts("someannoyingspam")
+```
+
--- blockr/tumblr.py
+++ blockr/tumblr.py
@@ -3,13 +3,17 @@ import requests, json
 API_BASEURL = "https://api.tumblr.com/v2"
 API_ENDPOINTS = {
     "blog": "/blog/%s/%s?api_key=%s",
-    "tagged": "/tagged?tag=%s"
+    "tagged": "/tagged?tag=%s&api_key=%s"
 }
 
 
 class Tumblr:
     def __init__(self, api_key):
         self.api_key = api_key
+
+    def tagged(self, tag):
+        url = self.api_url("tagged") % (tag, self.api_key)
+        return self.get_request(url)
 
     def posts(self, blog_identifier):
         url = self.api_url("blog") % (blog_identifier,
